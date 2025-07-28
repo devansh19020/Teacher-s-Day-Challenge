35e985c6db112e7d12ab84aba25d1a5f047fd9cf
devansh19020 <devanshrawat1925@gmail.com>
2025-07-28 06:23:35 +0530

    Day 4 Code

---
+++ Day4/StockBuySell.java
@@ -0,0 +1,15 @@
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0. */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int b = prices[0];
+        int p = 0;
+        for (int i = 1; i < prices.length; i++) {
+        if (b > prices[i]) b = prices[i];
+        else p = Math.max(p, prices[i] - b);
+    }
+    return p;
+    }
+}
