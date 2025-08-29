fb4d05df6534c33292df31bbd3ffa02276dc7d0d
devansh19020 <devanshrawat1925@gmail.com>
2025-08-29 07:07:57 +0530

    Day 36 Code

---
+++ Day36/LuckyDivision.java
@@ -0,0 +1,25 @@
+/* Petya loves lucky numbers. Everybody knows that lucky numbers are positive integers whose decimal representation contains only the lucky digits 4 and 7. For example, numbers 47, 744, 4 are lucky and 5, 17, 467 are not.
+Petya calls a number almost lucky if it could be evenly divided by some lucky number. Help him find out if the given number n is almost lucky. */
+
+import java.util.Scanner;
+
+public class LuckyDivision {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        int[] luckyNumbers = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+        boolean isAlmostLucky = false;
+        for (int lucky : luckyNumbers) {
+            if (n % lucky == 0) {
+                isAlmostLucky = true;
+                break;
+            }
+        }
+        if (isAlmostLucky) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
